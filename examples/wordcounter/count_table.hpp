eb744cabf9437ef6bc0b4f292588c0031b3e3534
Keita Iwabuchi <kiwabuchi@llnl.gov>
2021-03-06 03:28:45 -0800

    Add word count programs that use Metall

---
+++ examples/wordcounter/count_table.hpp
@@ -0,0 +1,20 @@
+// Copyright 2020 Lawrence Livermore National Security, LLC and other CLIPPy Project Developers.
+// See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: MIT
+
+#ifndef CLIPPY_EXAMPLES_WORDCOUNTER_COUNT_TABLE_HPP
+#define CLIPPY_EXAMPLES_WORDCOUNTER_COUNT_TABLE_HPP
+
+#include <functional>
+#include <metall/container/string.hpp>
+#include <metall/container/unordered_map.hpp>
+#include <metall/utility/hash.hpp>
+
+using table_value_type = std::pair<metall::container::string, clippy::integer>;
+using count_table_type = metall::container::unordered_map<metall::container::string, int,
+                                                          metall::utility::string_hash<metall::container::string>,
+                                                          std::equal_to<metall::container::string>,
+                                                          metall::manager::scoped_allocator_type<table_value_type>>;
+
+#endif //CLIPPY_EXAMPLES_WORDCOUNTER_COUNT_TABLE_HPP
---
+++ examples/wordcounter/count_words.cpp
@@ -0,0 +1,45 @@
+// Copyright 2020 Lawrence Livermore National Security, LLC and other CLIPPy Project Developers.
+// See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: MIT
+
+#include <clippy/clippy.hpp>
+#include <metall/metall.hpp>
+#include "count_table.hpp"
+
+namespace mtl = metall;
+
+int main(int argc, char **argv) {
+  clippy::clippy clip("count_words", "Count words");
+  clip.add_required<clippy::string>("path","Data store path");
+  clip.add_required<clippy::arraystring>("words","Unordered array of words");
+
+  clip.returns<clippy::integer>("Total words");
+  if (clip.parse(argc, argv)) { return 0; }
+
+  auto words = clip.get<clippy::arraystring>("words");
+  auto path = clip.get<clippy::string>("path");
+
+  std::unique_ptr<mtl::manager> manager;
+  if (mtl::manager::consistent(path.c_str())) { // Does the data store already exist?
+    // Open an existing data store
+    manager = std::make_unique<mtl::manager>(mtl::open_only, path.c_str());
+  } else {
+    // Create a new data store
+    manager = std::make_unique<mtl::manager>(mtl::create_only, path.c_str());
+  }
+  // Find the existing table or construct a new one
+  auto table = manager->find_or_construct<count_table_type>(mtl::unique_instance)(manager->get_allocator());
+
+  // Count words
+  for (const auto &word : words) {
+    auto pos = table->try_emplace(mtl::container::string(word.c_str(), table->get_allocator()), 0);
+    ++(pos.first->second);
+  }
+
+  int num_total_words = 0;
+  for (const auto& elem : *table) num_total_words += elem.second;
+  clip.to_return(num_total_words);
+
+  return 0;
+}
