c1e0b2279bc25d77ec0bab2db76cd6f8a45b3906
Keita Iwabuchi <iwabuchi1@llnl.gov>
2021-09-20 10:14:15 -0700

    Replace nlohmann::json with Boost.JSON

--- include/clippy/clippy.hpp
+++ include/clippy/clippy.hpp
@@ -11,7 +11,9 @@
 #include <functional>
 #include <iostream>
 #include <sstream>
-#include <nlohmann/json.hpp>
+#include <utility>
+#include <boost/json/src.hpp>
+
 
 namespace clippy {
 using integer = int64_t;
@@ -21,6 +23,7 @@ using string = std::string;
 using arraystring = std::vector<string>;
 using arrayint = std::vector<int64_t>;
 using arrayintint = std::vector<std::pair<int64_t, int64_t>>;
+
 namespace detail {
 template <typename T>
 struct name_of_type : std::false_type {
@@ -75,8 +78,8 @@ inline std::string get_type_name() {
 class clippy {
  public:
   clippy(const std::string &&name, const std::string &&desc) {
-    m_json_config["method_name"] = name;
-    m_json_config["desc"] = desc;
+    get_value(m_json_config, "method_name") = name;
+    get_value(m_json_config, "desc") = desc;
   }
 
   ~clippy() {
@@ -87,25 +90,25 @@ class clippy {
   void add_required(const std::string &&name, const std::string &&desc) {
     add_required_validator<T>(name);
     size_t position = m_next_position++;
-    m_json_config["args"][name]["type"] = detail::get_type_name<T>();
-    m_json_config["args"][name]["desc"] = desc;
-    m_json_config["args"][name]["position"] = position;
+    get_value(m_json_config, "args", name, "type") = detail::get_type_name<T>();
+    get_value(m_json_config, "args", name, "desc") = desc;
+    get_value(m_json_config, "args", name, "position") = position;
   }
 
   template <typename T>
   void add_optional(const std::string &&name, const std::string &&desc,
                     const T &default_val) {
     add_optional_validator<T>(name);
-    m_json_config["args"][name]["type"] = detail::get_type_name<T>();
-    m_json_config["args"][name]["desc"] = desc;
-    m_json_config["args"][name]["position"] = -1;
-    m_json_config["args"][name]["default_val"] = default_val;
+    get_value(m_json_config, "args", name, "type") = detail::get_type_name<T>();
+    get_value(m_json_config, "args", name, "desc") = desc;
+    get_value(m_json_config, "args", name, "position") = -1;
+    get_value(m_json_config, "args", name, "default_val") = default_val;
   }
 
   template <typename T>
   void returns(const std::string &&desc) {
-    m_json_config["returns"]["type"] = detail::get_type_name<T>();
-    m_json_config["returns"]["desc"] = desc;
+    get_value(m_json_config, "returns", "type") = detail::get_type_name<T>();
+    get_value(m_json_config, "returns", "desc") = desc;
   }
 
   template <typename T>
@@ -115,7 +118,7 @@ class clippy {
     //   throw std::runtime_error("clippy::to_return(value):  Invalid type.");
     // }
     return_values = true;
-    m_json_return = value;
+    m_json_return = boost::json::value_from(value);
   }
 
   bool parse(int argc, char **argv) {
@@ -125,7 +128,9 @@ class clippy {
       std::cout << m_json_config;
       return true;
     }
-    std::cin >> m_json_input;
+    std::string buf;
+    std::cin >> buf;
+    m_json_input = boost::json::parse(buf);
     validate_json_input();
 
     if (argc == 2 && std::string(argv[1]) == DRYRUN_FLAG) { return true; }
@@ -138,19 +143,18 @@ class clippy {
   T get(const std::string &&name) {
     if (has_argument(name)) {  // if the argument exists
       if (detail::get_type_name<T>() !=
-          m_json_config["args"][name]["type"].get<std::string>()) {
+          get_value(m_json_config, "args", name, "type").get_string()) {
         throw std::runtime_error("clippy::get(name):  Invalid type.");
       }
-      return m_json_input[name].get<T>();
+      return boost::json::value_to<T>(get_value(m_json_input, name));
     } else {  // it's an optional
-              // std::cout << "optional argument found: " + name << std::endl;
-      return m_json_config["args"][name]["default_val"].get<T>();
+      // std::cout << "optional argument found: " + name << std::endl;
+      return boost::json::value_to<T>(get_value(m_json_config, "args", name, "default_val"));
     }
   }
 
   bool has_argument(const std::string &name) {
-    if (m_json_input.count(name) == 0) { return false; }
-    return true;
+    return m_json_input.as_object().contains(name);
   }
 
  private:
@@ -167,10 +171,10 @@ class clippy {
          << "\n";
       throw std::runtime_error(ss.str());
     }
-    m_input_validators[name] = [name](const nlohmann::json &j) {
-      if (j.count(name) == 0) { return; }  // Optional, only eval if present
+    m_input_validators[name] = [name](const boost::json::value &j) {
+      if (!j.get_object().contains(name)) { return; }  // Optional, only eval if present
       try {
-        j[name].get<T>();
+        boost::json::value_to<T>(get_value(j, name));
       } catch (const std::exception &e) {
         std::stringstream ss;
         ss << "CLIPPy ERROR:  Optional argument " << name << ": \"" << e.what()
@@ -185,14 +189,14 @@ class clippy {
     if (m_input_validators.count(name) > 0) {
       throw std::runtime_error("Clippy:: Cannot have duplicate argument names");
     }
-    m_input_validators[name] = [name](const nlohmann::json &j) {
-      if (j.count(name) == 0) {
+    m_input_validators[name] = [name](const boost::json::value &j) {
+      if (!j.as_object().contains(name)) {
         std::stringstream ss;
         ss << "CLIPPy ERROR:  Required argument " << name << " missing.\n";
         throw std::runtime_error(ss.str());
       }
       try {
-        j[name].get<T>();
+        boost::json::value_to<T>(get_value(j, name));
       } catch (const std::exception &e) {
         std::stringstream ss;
         ss << "CLIPPy ERROR:  Required argument " << name << ": \"" << e.what()
@@ -202,14 +206,63 @@ class clippy {
     };
   }
 
-  nlohmann::json m_json_config;
-  nlohmann::json m_json_input;
-  nlohmann::json m_json_return;
+  inline static boost::json::value &get_value(boost::json::value &value, const std::string &key) {
+    return value.get_object()[key];
+  }
+
+  template <typename ...argts>
+  inline static boost::json::value &get_value(boost::json::value &value,
+                                              const std::string &key,
+                                              const argts &... inner_keys) {
+    return get_value(value.get_object()[key], inner_keys...);
+  }
+
+  inline static const boost::json::value &get_value(const boost::json::value &value, const std::string &key) {
+    return value.get_object().at(key);
+  }
+
+  template <typename ...argts>
+  inline static const boost::json::value &get_value(const boost::json::value &value,
+                                                    const std::string &key,
+                                                    const argts &... inner_keys) {
+    return get_value(value.get_object().at(key), inner_keys...);
+  }
+
+  boost::json::value m_json_config;
+  boost::json::value m_json_input;
+  boost::json::value m_json_return;
   size_t m_next_position = 0;
 
   bool return_values = false;
 
-  std::map<std::string, std::function<void(const nlohmann::json &)>>
+  std::map<std::string, std::function<void(const boost::json::value &)>>
       m_input_validators;
 };
+
 }  // namespace clippy
+
+namespace boost::json {
+void tag_invoke(boost::json::value_from_tag, boost::json::value &jv, const clippy::arrayintint &value) {
+  auto& outer_array = jv.emplace_array();
+  outer_array.resize(value.size());
+
+  for (std::size_t i = 0; i < value.size(); ++i) {
+    auto& inner_array = outer_array[i].emplace_array();
+    inner_array.resize(2);
+    inner_array[0] = value[i].first;
+    inner_array[1] = value[i].second;
+  }
+}
+
+clippy::arrayintint tag_invoke(boost::json::value_to_tag<clippy::arrayintint>, const boost::json::value &jv) {
+  clippy::arrayintint value;
+
+  auto& outer_array = jv.get_array();
+  for (const auto& inner_value : outer_array) {
+    const auto& inner_array = inner_value.get_array();
+    value.emplace_back(std::make_pair(inner_array[0].as_int64(),inner_array[1].as_int64()));
+  }
+
+  return value;
+}
+}
