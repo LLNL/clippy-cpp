07c8aff88b4bc3acd15e77787d62b73d743a066a
Seth Bromberger <github@bromberger.com>
2024-02-15 18:00:58 -0800

    fixes bag

--- test/clippy_bag/clippy_bag_remove_if.cpp
+++ test/clippy_bag/clippy_bag_remove_if.cpp
@@ -1,13 +1,12 @@
-// Copyright 2021 Lawrence Livermore National Security, LLC and other CLIPPy Project Developers.
-// See the top-level COPYRIGHT file for details.
+// Copyright 2021 Lawrence Livermore National Security, LLC and other CLIPPy
+// Project Developers. See the top-level COPYRIGHT file for details.
 //
 // SPDX-License-Identifier: MIT
 
-#include <iostream>
-#include <boost/json.hpp>
-#include <list>
 #include "clippy/clippy.hpp"
-
+#include <boost/json.hpp>
+#include <iostream>
+#include <list>
 
 namespace boostjsn = boost::json;
 
@@ -15,23 +14,27 @@ static const std::string class_name = "ClippyBag";
 static const std::string method_name = "remove_if";
 static const std::string state_name = "INTERNAL";
 
-
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
   clippy::clippy clip{method_name, "Initializes a ClippyBag of strings"};
 
   clip.member_of("ClippyBag", "Example bag container");
   clip.add_required<std::string>("item", "Item to remove");
-  clip.add_required_state<std::list<std::string>>(state_name, "Internal container");
+  clip.add_required_state<std::list<std::string>>(state_name,
+                                                  "Internal container");
 
   // no object-state requirements in constructor
-  if (clip.parse(argc, argv)) { return 0; }
-  
+  if (clip.parse(argc, argv)) {
+    return 0;
+  }
+
   auto item = clip.get<std::string>("item");
   auto the_bag = clip.get_state<std::list<std::string>>(state_name);
-  the_bag.remove(item);
+  for (auto it = the_bag.begin(); it != the_bag.end(); ++it) {
+    if (*it == item) {
+      the_bag.erase(it);
+      break;
+    }
+  }
   clip.set_state(state_name, the_bag);
   return 0;
 }
-
-
