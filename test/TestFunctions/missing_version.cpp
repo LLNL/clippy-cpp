346ebe75521aee97edc4ce28041e0374de5dae6f
Seth Bromberger <github@bromberger.com>
2024-03-30 12:40:36 -0700

    changed method names

--- test/TestFunctions/missing_version.cpp
+++ test/TestFunctions/missing_version.cpp
@@ -2,6 +2,6 @@
 
 int main(int argc, char **arv) {
   std::cout
-      << R"json({"method_name":"returns_string","desc":"Tests returning a string","returns":{"desc":"The string"}})json";
+      << R"json({"method_name":"missing_version","desc":"Tests returning a string - missing a version spec","returns":{"desc":"The string"}})json";
   return 0;
 }
