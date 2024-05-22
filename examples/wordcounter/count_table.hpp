1904b6cda7fce4bb53e10a9acbf5bebe4f4efeb7
Keita Iwabuchi <iwabuchi1@lln.gov>
2024-05-22 12:34:24 -0700

    Fix build error

--- examples/wordcounter/count_table.hpp
+++ examples/wordcounter/count_table.hpp
@@ -1,5 +1,5 @@
-// Copyright 2020 Lawrence Livermore National Security, LLC and other CLIPPy Project Developers.
-// See the top-level COPYRIGHT file for details.
+// Copyright 2020 Lawrence Livermore National Security, LLC and other CLIPPy
+// Project Developers. See the top-level COPYRIGHT file for details.
 //
 // SPDX-License-Identifier: MIT
 
@@ -12,9 +12,9 @@
 #include <metall/utility/hash.hpp>
 
 using table_value_type = std::pair<metall::container::string, int>;
-using count_table_type = metall::container::unordered_map<metall::container::string, int,
-                                                          metall::utility::string_hash<metall::container::string>,
-                                                          std::equal_to<metall::container::string>,
-                                                          metall::manager::scoped_allocator_type<table_value_type>>;
+using count_table_type = metall::container::unordered_map<
+    metall::container::string, int, metall::utility::str_hash<>,
+    std::equal_to<metall::container::string>,
+    metall::manager::scoped_allocator_type<table_value_type>>;
 
-#endif //CLIPPY_EXAMPLES_WORDCOUNTER_COUNT_TABLE_HPP
+#endif // CLIPPY_EXAMPLES_WORDCOUNTER_COUNT_TABLE_HPP
