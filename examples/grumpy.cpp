98fd7b6d6947a0ecfe59c816d61e58c38e691f86
Seth Bromberger <sbromberger@users.noreply.github.com>
2024-09-20 07:31:47 -0700

    Sbromberger/fix examples (#36)
    * fixing examples so that they build.

--- examples/grumpy.cpp
+++ examples/grumpy.cpp
@@ -1,15 +1,13 @@
-// Copyright 2019 Lawrence Livermore National Security, LLC and other Clippy Project Developers.
-// See the top-level COPYRIGHT file for details.
+// Copyright 2019 Lawrence Livermore National Security, LLC and other Clippy
+// Project Developers. See the top-level COPYRIGHT file for details.
 //
 // SPDX-License-Identifier: MIT
 
 #include <clippy/clippy.hpp>
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   clippy::clippy clip("grumpy", "Always throws errors because he's Grumpy!");
-  if (clip.parse(argc, argv))
-  {
+  if (clip.parse(argc, argv)) {
     return 0;
   }
 
